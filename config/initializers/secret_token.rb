77d5e31bd20c25ea14229b6b4eff64277d3c7088
Alex Hollweg <alexhollweg@t-online.de>
2013-08-14 17:13:27 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PdfLib::Application.config.secret_key_base = '8b6282778cb9a90edf56a3364b835b44518e9e0dece907a75b6f21990a138087ed9af810a12016f361fb4eb9097cad12c2dbc0dd767a0dd849923abc49b55035'
