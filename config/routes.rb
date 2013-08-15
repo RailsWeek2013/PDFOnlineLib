2c718e982eefa46a7f44f93dc625f78ea848f026
Alex Hollweg <alexhollweg@t-online.de>
2013-08-15 13:34:56 +0200

    First version of User-Management added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 PdfLib::Application.routes.draw do
+  get "pages/home"
   devise_for :users
+
+  root to: "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
